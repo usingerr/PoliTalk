663bd04238e38916c22d30c85c51b498a431b0b9
Glenn <40702143+priceg@users.noreply.github.com>
2019-03-31 17:03:22 -0400

    Made progress on User Section and Search Function
    There is still more work to be done in these sections.

--- scripts/PoliTalk.js
+++ scripts/PoliTalk.js
@@ -22,4 +22,54 @@ function newArticle() {
 	articles.innerHTML = articleForm;
 }
 
+function newDiscussion()
+{
+	alert("Discussion button works");
+}
+
+function logOut()
+{
+	alert("log out button works");
+}
+
+var placeHolderQueryResult = ["Donald Trump", "Donald Glover", "Hillary Clinton", "Michelle Obama", "Kirsten Gillibrand", "Beto O'Rourke", "John Hickenlooper", "Jay Inslee", "Bernie Sanders", "Amy Klobuchar", "Elizabeth Warren", "Cory Booker", "Kamala Harris", "Julian Castro", "Tulsi Gabbard", "John Delaney", "Wayne Messam", "Marianne Williamson", "Andrew Yang", "Pete Buttigieg", "Gonzalo Barrios", "Marco Rubio", "Ted Cruz", "George Bush", "Kanye West", "Dwayne Johnson", "Joanne Rowling"]
+
+function update(userList)
+{
+	for(var y = 0; y < userList.length; y++)
+	{
+		//alert(userList[y]);
+	}
+	var physicalList = document.getElementById("userList");
+	while(physicalList.hasChildNodes())
+	{
+		physicalList.removeChild(physicalList.childNodes[0]);
+	}
+	var defaultLength = 10;
+	if(userList.length < defaultLength)
+		defaultLength = userList.length;
+	for(var x = 0; x < defaultLength; x++)
+	{
+		var result = document.createElement("li");
+		var resultText = document.createTextNode(userList[x]);
+		result.appendChild(resultText);
+		physicalList.appendChild(result);
+	}
+}
+
+function  updateText()
+{
+	var input = document.getElementById("userSearch").value;
+	var newList = [];
+	for(var x = 0; x < placeHolderQueryResult.length; x++)
+	{
+		//alert("checking querey result " + placeHolderQueryResult[x].substring(0, input.length) + " with " + input);
+		if(placeHolderQueryResult[x].substring(0, input.length) == input){
+			
+			newList.push(placeHolderQueryResult[x]);
+		}
+	}
+	update(newList)
+}
+
 window.onload=defaultTab;
