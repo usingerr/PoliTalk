6e66a66774ac0b7cb9ea60a6c24a3a112b74482c
usingerr <usingerr@wit.edu>
2019-04-04 15:16:16 -0700

    added ability to add and remove sources (source ids are 'source' + the number source it is, starting with 0)

--- scripts/PoliTalk.js
+++ scripts/PoliTalk.js
@@ -22,54 +22,69 @@ function newArticle() {
 	articles.innerHTML = articleForm;
 }
 
-function newDiscussion()
-{
-	alert("Discussion button works");
-}
-
-function logOut()
-{
-	alert("log out button works");
-}
-
-var placeHolderQueryResult = ["Donald Trump", "Donald Glover", "Hillary Clinton", "Michelle Obama", "Kirsten Gillibrand", "Beto O'Rourke", "John Hickenlooper", "Jay Inslee", "Bernie Sanders", "Amy Klobuchar", "Elizabeth Warren", "Cory Booker", "Kamala Harris", "Julian Castro", "Tulsi Gabbard", "John Delaney", "Wayne Messam", "Marianne Williamson", "Andrew Yang", "Pete Buttigieg", "Gonzalo Barrios", "Marco Rubio", "Ted Cruz", "George Bush", "Kanye West", "Dwayne Johnson", "Joanne Rowling"]
-
-function update(userList)
-{
-	for(var y = 0; y < userList.length; y++)
-	{
-		//alert(userList[y]);
+function addSource() {
+	// number of source elements to append
+	var childCount = ((document.getElementById("sourceContainer").childElementCount)/2);
+	// container <div> where dynamic content will be placed
+	var container = document.getElementById("sourceContainer");
+	// values for current sources
+	var values = [];
+	for (i = 0; i < childCount; i++) {
+		console.log("source" + i);
+		values[i] = document.getElementById("source" + i).value;
 	}
-	var physicalList = document.getElementById("userList");
-	while(physicalList.hasChildNodes())
-	{
-		physicalList.removeChild(physicalList.childNodes[0]);
+	// clear previous contents of the container
+	while (container.hasChildNodes()) {
+		container.removeChild(container.lastChild);
 	}
-	var defaultLength = 10;
-	if(userList.length < defaultLength)
-		defaultLength = userList.length;
-	for(var x = 0; x < defaultLength; x++)
-	{
-		var result = document.createElement("li");
-		var resultText = document.createTextNode(userList[x]);
-		result.appendChild(resultText);
-		physicalList.appendChild(result);
-	}
-}
-
-function  updateText()
-{
-	var input = document.getElementById("userSearch").value;
-	var newList = [];
-	for(var x = 0; x < placeHolderQueryResult.length; x++)
-	{
-		//alert("checking querey result " + placeHolderQueryResult[x].substring(0, input.length) + " with " + input);
-		if(placeHolderQueryResult[x].substring(0, input.length) == input){
-			
-			newList.push(placeHolderQueryResult[x]);
+	for (i = 0 ; i <= childCount; i++){
+		// append source label
+		container.appendChild(document.createTextNode("Source " + (i+1) + " "));
+		// create an input element, set its type, name, and id
+		var input = document.createElement("input");
+		input.type = "text";
+		input.name = "source" + i;
+		input.id = "source" + i;
+		if (values[i] != '' && values[i] != null) {
+			input.value = values[i];
+		} else {
+			input.value = '';
 		}
+		// append source input box
+		container.appendChild(input);
+		
+		
+		//PROBABLY DELETE THIS SHIT VVVVVVVVVVV
+		/*// create removeSource button
+		var remove = document.createElement("button");
+		remove.type = "button";
+		remove.innerHTML = "<img src='images\\remove.png' id='removeImg' />";
+		remove.onclick = removeSource(this.id);
+		remove.style = "height: 10px; width: 10px;";
+		remove.id = "remove" + i;
+		// append remove button
+		container.appendChild(remove); */
+		//PROBABLY DELETE THIS SHIT ^^^^^^^^^^^^
+		
+		
+		// append line break
+		container.appendChild(document.createElement("br"));
 	}
-	update(newList)
+}
+
+function removeSource(buttonID) {
+	// get source container object
+	container = document.getElementById("sourceContainer");
+	// get count of its children
+	childCount = (container.childElementCount/2);
+	if (childCount == 1) {
+		return false;
+	} else {
+		container.removeChild(container.lastChild);
+		container.removeChild(container.lastChild);
+		container.removeChild(container.lastChild);
+		return true;
+	}
 }
 
 window.onload=defaultTab;
